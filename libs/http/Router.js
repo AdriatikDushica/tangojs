83d2113db150fecc83e8d77346798ebe751243a2
Adriatik Dushica <adriatikdushica@MacBook-Air-di-Adriatik.local>
2015-10-21 19:42:37 +0200

    added comments

--- libs/http/Router.js
+++ libs/http/Router.js
@@ -9,34 +9,55 @@ var Router = function() {
         'delete': {}
     }
 
+    /**
+     * Define a new GET route
+     */
     this.get = function(url, fnc) {
-    	routes['get'][ stack.join('') + url ] = fnc;
+        routes['get'][ stack.join('') + url ] = fnc;
     }
 
+    /**
+     * Define a new POST route
+     */
     this.post = function(url, fnc) {
-    	routes['post'][ stack.join('') + url ] = fnc;
+        routes['post'][ stack.join('') + url ] = fnc;
     }
 
+    /*
+     * Define a new PUT route
+     */
     this.put = function(url, fnc) {
-    	routes['put'][ stack.join('') + url ] = fnc;
+        routes['put'][ stack.join('') + url ] = fnc;
     }
 
+    /*
+     * Define a new DELETE route
+     */
     this.delete = function(url, fnc) {
-    	routes['delete'][ stack.join('') + url ] = fnc;
+        routes['delete'][ stack.join('') + url ] = fnc;
     }
 
+    /*
+     * Define a group
+     */
     this.group = function(params, fnc) {
-    	stack.push(params.prefix);
-    	fnc();
-    	stack.pop();
+        stack.push(params.prefix);
+        fnc();
+        stack.pop();
     }
 
+    /*
+     * Returns the content of a route
+     */
     this.run = function(method, url) {
-    	return routes[method][url]();
+        return routes[method][url]();
     }
 
+    /*
+     * Handle the request and the response
+     */
     this.handle = function(request, response) {
-    	
+        
     }
 
 }
